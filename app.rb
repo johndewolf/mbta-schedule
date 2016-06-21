a321480c1559ed694fab7ba8f80c75e3f2ba76e2
John DeWolf <jdewolf@backupify.com>
2016-06-21 13:00:53 -0400

    small adjustments to table

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ get '/' do
 	response = CSV.new(HTTParty.get('http://developer.mbta.com/lib/gtrtfs/Departures.csv').body, :headers => true, :header_converters => :symbol, :converters => :all)
 	@tripData = response.to_a.map {|row|
 		row[0] = DateTime.strptime(getDate(row[0]), "%s").strftime("%H:%M:%S EST")
-		row[4] = DateTime.strptime(getDate(row[4]), "%s").strftime("%H:%M:%S EST")
+		row[4] = DateTime.strptime(getDate(row[4]), "%s").strftime("%H:%M EST")
 		if row[5] > 0
 			row[5] = row[5] / 60
 		end
